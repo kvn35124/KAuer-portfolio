70844b2026e102f7fc8077e68b28d57795cc2c1c
Kevin Auer <kvn35124@uab.edu>
2019-11-05 08:29:05 -0600

    added some badges to projects page

--- src/client/Pages/Work.tsx
+++ src/client/Pages/Work.tsx
@@ -15,7 +15,8 @@ class Work extends React.Component<IWorkProps, IWorkState> {
                             <div className="card-body">
                                 <h3 className="font-weight-bold text-center">Event Master 3000</h3>
                                 <p className="text-center">Event Master 3000 allows users to sign in and create events. Events can be edited or deleted and events are automatically deleted once the event date has passed.</p>
-                                <p className="text-center"><span className="badge badge-dark mx-1">React</span><span className="badge badge-dark mx-1">Node.JS</span><span className="badge badge-dark mx-1">Passport.JS</span></p>
+                                <p className="text-center"><span className="badge badge-dark mx-1">React</span><span className="badge badge-dark mx-1">Node.JS</span><span className="badge badge-dark mx-1">Passport.JS</span><span className="badge badge-dark mx-1">MySQL</span><span className="badge badge-dark mx-1">Express.JS</span></p>
+                                <p><span className="badge border border-dark mx-1">View Source Code</span><span className="badge border border-dark mx-1">View Live</span></p>
                             </div>
                         </div>
                     </section>
